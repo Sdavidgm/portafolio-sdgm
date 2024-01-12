15d7d70e83bb1b441b2783f51127ce31bc1a978b
Santiago Guamanquispe <santiago_davidgm@hotmail.com>
2024-01-11 23:01:38 -0500

    agrego testimonios

--- assest/js/index.js
+++ assest/js/index.js
@@ -32,3 +32,31 @@ for (const skill of skills) {
 }
 
 skillsTech.innerHTML = html
+
+/*navbar*/
+
+const navbar = document.querySelector('.nav')
+const menu = document.querySelector('.nav-menu')
+const links = document.querySelectorAll('.list-link')
+
+navbar.addEventListener('click', function (e) {
+  if (e.target.closest('.btn-menu')) {
+    menu.classList.toggle('show-menu')
+  }
+
+  if (e.target.closest('.btn-close')) {
+    menu.classList.remove('show-menu')
+  }
+
+  if (e.target.closest('.lista-link')) {
+    menu.classList.remove('show-menu')
+  }
+
+  if (e.target.closest('.lista-link')) {
+    for (const link of links) {
+      link.parentElement.classList.remove('active')
+    }
+
+    e.target.parentElement.classList.add('active')
+  }
+})
